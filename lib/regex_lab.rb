d50737fac5e3d1d6eae913237733804b52182919
David Ritchey <ritchey0713@gmail.com>
2018-03-27 21:57:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,13 +6,10 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
  text.scan(/\b[Uu]n[a-z]*ing\b/ )
 
- 
- 
- 
 end
 
 def words_five_letters_long(text)
-
+text.
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
